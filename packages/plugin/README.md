3e7e3d9b6fc81bbe81004b1a70d9b23e7aa69b11
leegeunhyeok <dev.ghlee@gmail.com>
2025-02-06 04:15:23 +0900

    docs: update README

--- packages/plugin/README.md
+++ packages/plugin/README.md
@@ -1,1 +1,68 @@
 # plugin
+
+## Installation
+
+```bash
+# npm
+npm install @esdoctor/plugin
+
+# pnpm
+pnpm add @esdoctor/plugin
+
+# yarn
+yarn add @esdoctor/plugin
+```
+
+## Usage
+
+```ts
+import { create } from '@esdoctor/plugin';
+
+const esdoctor = create();
+
+esbuild.build({
+  /* Other options */
+  plugins: [
+    // The base plugin, which should be placed first in the array.
+    esdoctor.plugin,
+    // Bound: will be traced by esdoctor for analysis
+    esdoctor.bind(traceablePlugin1),
+    esdoctor.bind(traceablePlugin2),
+    esdoctor.bind(traceablePlugin3),
+    // Unbound: will not be traced (plain plugin)
+    otherPlugin,
+  ],
+  // Should enable `metafile` option to get the analysis result
+  metafile: true,
+});
+```
+
+## API
+
+### create
+
+Create a plugin context.
+
+```ts
+// Signature
+
+function create(options?: PluginOptions): {
+  plugin: esbuild.Plugin;
+  bind: (plugin: esbuild.Plugin) => esbuild.Plugin;
+};
+
+interface PluginOptions {
+  enabled?: boolean;
+}
+```
+
+**Parameters:**
+
+- `options`: The options for the plugin.
+  - `enabled`: Enable the esdoctor plugin. (default: `true`)
+
+**Returns:**
+
+- `{ plugin: esbuild.Plugin, bind: (plugin: esbuild.Plugin) => esbuild.Plugin }`: The plugin context.
+  - `plugin`: The base plugin of esdoctor.
+  - `bind(plugin)`: Returns an enhanced plugin so that it can be traced by esdoctor for analysis.
--- packages/plugin/src/index.spec.ts
+++ packages/plugin/src/index.spec.ts
@@ -18,7 +18,7 @@ const TEMPLATE = {
   }),
 };
 
-describe('esdoctor-plugin', () => {
+describe('@esdoctor/plugin', () => {
   function getBaseBuildOptions(): esbuild.BuildOptions {
     return {
       stdin: { contents: TEMPLATE.entryScript },
@@ -30,78 +30,146 @@ describe('esdoctor-plugin', () => {
     };
   }
 
-  it('should extend esbuild result', async () => {
-    const esdoctor = create();
-    const result = await esbuild.build({
-      ...getBaseBuildOptions(),
-      plugins: [esdoctor.plugin, esdoctor.bind(TEMPLATE.fooResolver())],
-    });
-
-    expect('environment' in result).toBe(true);
-    expect('startedAt' in result).toBe(true);
-    expect('endedAt' in result).toBe(true);
-    expect('duration' in result).toBe(true);
-    expect('traces' in result).toBe(true);
-    expect('initialOptions' in result).toBe(true);
-  });
-
-  describe('when build with bound plugins', () => {
-    it('should trace the bound plugin', async () => {
+  describe('when enabled', () => {
+    it('should extend esbuild result', async () => {
       const esdoctor = create();
-      const nonBoundPlugin = {
-        name: 'non-bound-plugin',
-        setup(build) {
-          build.onStart(() => {});
-          build.onEnd(() => {});
-        },
-      };
-
-      const boundPlugin = {
-        name: 'bound-plugin',
-        setup(build) {
-          build.onStart(() => {});
-          build.onEnd(() => {});
-        },
-      };
-
-      const result = (await esbuild.build({
+      const result = await esbuild.build({
         ...getBaseBuildOptions(),
-        plugins: [
-          esdoctor.plugin,
-          TEMPLATE.fooResolver(),
-          // non-bound plugin
-          nonBoundPlugin,
-          // bound plugin
-          esdoctor.bind(boundPlugin),
-        ],
-      })) as BuildResult;
+        plugins: [esdoctor.plugin, esdoctor.bind(TEMPLATE.fooResolver())],
+      });
 
-      const traceNames = result.traces.map((trace) => trace.name);
-      const hasNonBoundPluginTrace = traceNames.some((name) =>
-        name.includes('non-bound-plugin')
-      );
+      expect('environment' in result).toBe(true);
+      expect('startedAt' in result).toBe(true);
+      expect('endedAt' in result).toBe(true);
+      expect('duration' in result).toBe(true);
+      expect('traces' in result).toBe(true);
+      expect('initialOptions' in result).toBe(true);
+    });
 
-      expect(hasNonBoundPluginTrace).toBe(false);
-      expect(traceNames).toMatchInlineSnapshot(`
-        [
-          "bound-plugin@onStart",
-          "bound-plugin@onEnd",
-        ]
-      `);
+    describe('when `metafile` is disabled', () => {
+      it('should throw an error', async () => {
+        const esdoctor = create();
+
+        await expect(() =>
+          esbuild.build({
+            ...getBaseBuildOptions(),
+            metafile: false,
+            plugins: [esdoctor.plugin, esdoctor.bind(TEMPLATE.fooResolver())],
+          })
+        ).rejects.toThrowError();
+      });
+    });
+
+    describe('when build with bound plugins', () => {
+      it('should trace the bound plugin', async () => {
+        const esdoctor = create();
+        const nonBoundPlugin = {
+          name: 'non-bound-plugin',
+          setup(build) {
+            build.onStart(() => {});
+            build.onEnd(() => {});
+          },
+        };
+
+        const boundPlugin = {
+          name: 'bound-plugin',
+          setup(build) {
+            build.onStart(() => {});
+            build.onEnd(() => {});
+          },
+        };
+
+        const result = (await esbuild.build({
+          ...getBaseBuildOptions(),
+          plugins: [
+            esdoctor.plugin,
+            TEMPLATE.fooResolver(),
+            // non-bound plugin
+            nonBoundPlugin,
+            // bound plugin
+            esdoctor.bind(boundPlugin),
+          ],
+        })) as BuildResult;
+
+        const traceNames = result.traces.map((trace) => trace.name);
+        const hasNonBoundPluginTrace = traceNames.some((name) =>
+          name.includes('non-bound-plugin')
+        );
+
+        expect(hasNonBoundPluginTrace).toBe(false);
+        expect(traceNames).toMatchInlineSnapshot(`
+          [
+            "bound-plugin@onStart",
+            "bound-plugin@onEnd",
+          ]
+        `);
+      });
     });
   });
 
-  describe('when `metafile` is disabled', () => {
-    it('should throw an error', async () => {
-      const esdoctor = create();
+  describe('when disabled', () => {
+    it('should not extend esbuild result', async () => {
+      const esdoctor = create({ enabled: false });
+      const result = await esbuild.build({
+        ...getBaseBuildOptions(),
+        plugins: [esdoctor.plugin, esdoctor.bind(TEMPLATE.fooResolver())],
+      });
 
-      expect(() =>
-        esbuild.build({
+      expect('environment' in result).toBe(false);
+      expect('startedAt' in result).toBe(false);
+      expect('endedAt' in result).toBe(false);
+      expect('duration' in result).toBe(false);
+      expect('traces' in result).toBe(false);
+      expect('initialOptions' in result).toBe(false);
+    });
+
+    describe('when `metafile` is disabled', () => {
+      it('should not throw', async () => {
+        const esdoctor = create({ enabled: false });
+
+        await expect(
+          esbuild.build({
+            ...getBaseBuildOptions(),
+            metafile: false,
+            plugins: [esdoctor.plugin, esdoctor.bind(TEMPLATE.fooResolver())],
+          })
+        ).resolves.toBeDefined();
+      });
+    });
+
+    describe('when build with bound plugins', () => {
+      it('should not trace', async () => {
+        const esdoctor = create({ enabled: false });
+        const nonBoundPlugin = {
+          name: 'non-bound-plugin',
+          setup(build) {
+            build.onStart(() => {});
+            build.onEnd(() => {});
+          },
+        };
+
+        const boundPlugin = {
+          name: 'bound-plugin',
+          setup(build) {
+            build.onStart(() => {});
+            build.onEnd(() => {});
+          },
+        };
+
+        const result = (await esbuild.build({
           ...getBaseBuildOptions(),
-          metafile: false,
-          plugins: [esdoctor.plugin, esdoctor.bind(TEMPLATE.fooResolver())],
-        })
-      ).rejects.toThrowError();
+          plugins: [
+            esdoctor.plugin,
+            TEMPLATE.fooResolver(),
+            // non-bound plugin
+            nonBoundPlugin,
+            // bound plugin
+            esdoctor.bind(boundPlugin),
+          ],
+        })) as BuildResult;
+
+        expect(result.traces).toBeUndefined();
+      });
     });
   });
 });
