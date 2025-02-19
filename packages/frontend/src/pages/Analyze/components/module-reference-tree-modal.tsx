f59e326bde5632a770e4a6f580b0b5d85482fbb8
leegeunhyeok <dev.ghlee@gmail.com>
2025-02-20 01:00:51 +0900

    feat(frontend): add module reference stack modal

---
+++ packages/frontend/src/pages/Analyze/components/module-reference-tree-modal.tsx
@@ -0,0 +1,116 @@
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+} from '@/components/ui/dialog';
+import { bytesToText } from '@/src/utils/filesize';
+import type { ModuleTreeItem } from '@/src/utils/resolve-module-tree';
+import { CornerDownRight, CornerLeftDown } from 'lucide-react';
+
+export interface ModuleReferenceTreeModalProps {
+  open: boolean;
+  module: {
+    path: string;
+    originSize: number;
+    bundledSize: number;
+  };
+  referenceStack: ModuleTreeItem[];
+  onOpenChange: (open: boolean) => void;
+}
+
+export function ModuleReferenceTreeModal({
+  open,
+  module,
+  referenceStack,
+  onOpenChange,
+}: ModuleReferenceTreeModalProps) {
+  const { treeShakable, nonTreeShakable } = toTreeShakableGroup(referenceStack);
+
+  const renderModuleItem = (item: ModuleTreeItem) => {
+    return (
+      <div key={item.path} className="flex flex-row items-center gap-2 text-sm">
+        <CornerLeftDown size={16} className="mt-2" />
+        {renderModuleOriginal(item)}
+        <p className="text-muted-foreground text-xs">(From: {item.path})</p>
+      </div>
+    );
+  };
+
+  const renderModuleOriginal = (item: ModuleTreeItem) => {
+    switch (item.referenceType) {
+      case 'dynamic-import':
+        return <code>import('{item.original}')</code>;
+
+      case 'import-statement':
+        return <code>import '{item.original}'</code>;
+
+      case 'require-call':
+        return <code>require('{item.original}')</code>;
+
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <Dialog modal open={open} onOpenChange={onOpenChange}>
+      <DialogContent className="data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed top-[50%] left-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border p-6 shadow-lg duration-200 sm:max-w-[700px] sm:rounded-lg">
+        <DialogHeader>
+          <DialogTitle>Module reference stack</DialogTitle>
+        </DialogHeader>
+        <div className="flex flex-col gap-2">
+          {treeShakable.length ? (
+            <div className="flex flex-col gap-1 rounded-md bg-blue-100 p-2">
+              <p className="text-xs text-blue-500">Tree shakable</p>
+              {treeShakable.map(renderModuleItem)}
+            </div>
+          ) : null}
+          {nonTreeShakable.length ? (
+            <div className="flex flex-col gap-1 rounded-md bg-red-100 p-2">
+              <p className="text-xs text-red-500">Non-tree shakable</p>
+              {nonTreeShakable.map(renderModuleItem)}
+            </div>
+          ) : null}
+          <div className="bg-muted flex flex-col gap-2 rounded-md p-2">
+            <div className="flex flex-row items-center gap-2">
+              {module.path}
+            </div>
+            <div className="flex flex-col">
+              <p>Bundled size: {bytesToText(module.bundledSize)}</p>
+              <p>Original size: {bytesToText(module.originSize)}</p>
+            </div>
+          </div>
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+}
+
+const toTreeShakableGroup = (moduleTree: ModuleTreeItem[]) => {
+  let lastTreeShakableIndex = -1;
+  let treeShakable = false;
+
+  for (let i = 0; i < moduleTree.length; i++) {
+    treeShakable = false;
+
+    switch (moduleTree[i].referenceType) {
+      case 'dynamic-import':
+      case 'import-statement':
+        treeShakable = true;
+        lastTreeShakableIndex = i;
+        break;
+    }
+
+    if (treeShakable === false) {
+      break;
+    }
+  }
+
+  lastTreeShakableIndex += 1;
+
+  return {
+    treeShakable: moduleTree.slice(0, lastTreeShakableIndex),
+    nonTreeShakable: moduleTree.slice(lastTreeShakableIndex),
+  };
+};
--- packages/frontend/src/pages/Analyze/components/treemap-chart.tsx
+++ packages/frontend/src/pages/Analyze/components/treemap-chart.tsx
@@ -2,6 +2,11 @@ import { Chart } from '@/src/components/chart';
 import type { ChartData } from '@/src/utils/chart';
 import { useTreemapSeries } from '../hooks/use-treemap-series';
 import { tooltip } from '../tooltip';
+import { ModuleReferenceTreeModal } from './module-reference-tree-modal';
+import {
+  EMPTY_MODULE,
+  useModuleReferenceTreeModalState,
+} from '../hooks/use-module-reference-tree-modal-state';
 
 export interface TreemapChartProps {
   data: ChartData;
@@ -9,14 +14,24 @@ export interface TreemapChartProps {
 }
 
 export function TreemapChart({ data, options }: TreemapChartProps) {
+  const { chartRef, moduleItem, reset } = useModuleReferenceTreeModalState();
   const series = useTreemapSeries(data, options);
 
   return (
-    <Chart
-      className="h-[70vh] bg-neutral-50"
-      series={series}
-      options={chartOptions}
-    />
+    <>
+      <Chart
+        ref={chartRef}
+        className="h-[70vh] bg-neutral-50"
+        series={series}
+        options={chartOptions}
+      />
+      <ModuleReferenceTreeModal
+        open={Boolean(moduleItem)}
+        module={moduleItem?.module ?? EMPTY_MODULE}
+        referenceStack={moduleItem?.referenceStack ?? []}
+        onOpenChange={reset}
+      />
+    </>
   );
 }
 
