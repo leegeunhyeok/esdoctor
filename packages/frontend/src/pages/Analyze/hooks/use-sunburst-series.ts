ecf543fc8026c3ebf57c875d634291ed464dabd0
leegeunhyeok <dev.ghlee@gmail.com>
2025-02-20 02:17:36 +0900

    chore: fix git filenames

--- packages/frontend/src/pages/Analyze/hooks/use-sunburst-series.ts
+++
@@ -1,9 +0,0 @@
-import { useMemo } from 'react';
-import { getSunburstSeries, type ChartData } from '@/src/utils/chart';
-
-export function useSunburstSeries(
-  data: ChartData,
-  options?: echarts.SunburstSeriesOption,
-) {
-  return useMemo(() => getSunburstSeries(data, options), [data, options]);
-}
--- packages/frontend/src/pages/Analyze/hooks/use-treemap-series.ts
+++
@@ -1,9 +0,0 @@
-import { useMemo } from 'react';
-import { getTreemapSeries, type ChartData } from '@/src/utils/chart';
-
-export function useTreemapSeries(
-  data: ChartData,
-  options?: echarts.TreemapSeriesOption,
-) {
-  return useMemo(() => getTreemapSeries(data, options), [data, options]);
-}
