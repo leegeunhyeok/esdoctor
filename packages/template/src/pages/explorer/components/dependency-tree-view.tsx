644ace377b7f384810fb2d0d49dc53180af2d286
leegeunhyeok <dev.ghlee@gmail.com>
2025-03-23 19:34:42 +0900

    feat(template): add file tree explorer

---
+++ packages/template/src/pages/explorer/components/dependency-tree-view.tsx
@@ -0,0 +1,144 @@
+import { Button } from '@/components/ui/button';
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet';
+import { CornerDownRight, Network } from 'lucide-react';
+import { FileTree } from './file-tree';
+import { useState } from 'react';
+import { bytesToText } from '@/src/utils/filesize';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/ui/tooltip';
+import { Badge } from '@/components/ui/badge';
+import { cn } from '@/lib/utils';
+import { Metafile } from '@esdoctor/types';
+import { createFileTreeData } from '../helpers/create-file-tree-data';
+
+const data = createFileTreeData(
+  Object.keys(window.__esdoctorDataSource.metafile.inputs),
+);
+
+export function DependencyTreeView() {
+  const [selectedFile, setSelectedFile] = useState<string | null>(null);
+
+  const renderContent = () => {
+    const module = selectedFile
+      ? window.__esdoctorDataSource.metafile.inputs[selectedFile]
+      : null;
+
+    if (module) {
+      return (
+        <div className="flex flex-col gap-2 p-2">
+          <div className="flex w-full items-center justify-between">
+            <h2 className="text-lg font-semibold">Discover module</h2>
+            <Button
+              variant="outline"
+              size="sm"
+              onClick={() => setSelectedFile(null)}
+            >
+              Dismiss
+            </Button>
+          </div>
+          <div className="text-muted-foreground flex items-center gap-2 text-sm">
+            <p>{selectedFile}</p>
+            {module.format != null ? (
+              <Badge
+                variant="outline"
+                className={cn(
+                  module.format === 'esm'
+                    ? 'border-blue-300 bg-blue-100 text-blue-500'
+                    : 'border-yellow-300 bg-yellow-100 text-yellow-500',
+                )}
+              >
+                {module.format.toUpperCase()}
+              </Badge>
+            ) : null}
+            <Badge
+              variant="outline"
+              className="border-gray-300 bg-gray-100 text-gray-500"
+            >
+              {bytesToText(module.bytes)}
+            </Badge>
+          </div>
+          {renderDependencies(module.imports)}
+        </div>
+      );
+    } else {
+      return (
+        <div className="flex h-full items-center justify-center">
+          <p className="text-muted-foreground text-sm">
+            Select a file to discover its dependencies.
+          </p>
+        </div>
+      );
+    }
+  };
+
+  const renderDependencies = (
+    imports: Metafile['metafile']['inputs'][string]['imports'],
+  ) => {
+    return imports.length === 0 ? (
+      <div className="text-muted-foreground text-sm">
+        No dependencies found.
+      </div>
+    ) : (
+      <div className="flex flex-col gap-1">
+        {imports.map((value) => (
+          <Tooltip key={value.path}>
+            <TooltipTrigger asChild>
+              <div
+                className="hover:bg-muted/50 text-muted-foreground flex cursor-pointer items-center gap-2 rounded-md px-2 py-1"
+                onClick={() => setSelectedFile(value.path)}
+              >
+                <CornerDownRight size={16} />
+                <p className="text-sm">{value.original ?? value.path}</p>
+              </div>
+            </TooltipTrigger>
+            <TooltipContent>{value.path}</TooltipContent>
+          </Tooltip>
+        ))}
+      </div>
+    );
+  };
+
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button variant="outline">
+          <Network />
+          Dependency Tree
+        </Button>
+      </SheetTrigger>
+      <SheetContent className="max-w-[100%] gap-0 sm:max-w-[850px]">
+        <SheetHeader>
+          <SheetTitle>Dependency Tree</SheetTitle>
+          <SheetDescription>
+            Explore the dependency tree of your project.
+          </SheetDescription>
+        </SheetHeader>
+        <TooltipProvider>
+          <div className="border-border flex h-[calc(100%-82px)] border-t">
+            <div className="border-border h-full w-[280px] overflow-auto border-r pr-2">
+              <FileTree
+                data={data}
+                focus={selectedFile}
+                onFileClick={setSelectedFile}
+              />
+            </div>
+            <div className="h-full w-full overflow-auto p-2">
+              {renderContent()}
+            </div>
+          </div>
+        </TooltipProvider>
+      </SheetContent>
+    </Sheet>
+  );
+}
---
+++ packages/template/src/pages/explorer/components/file-tree.tsx
@@ -0,0 +1,172 @@
+import { useEffect, useMemo, useState } from 'react';
+import { ChevronDown, ChevronRight, File, Folder } from 'lucide-react';
+import { cn } from '@/lib/utils';
+
+export type GroupedPaths = (string | (string | GroupedPaths)[])[];
+
+export type FileTreeProps = {
+  data: GroupedPaths;
+  focus: string | null;
+  className?: string;
+  onFileClick: (path: string) => void;
+};
+
+export type FileTreeItem = {
+  name: string;
+  path: string;
+  children?: FileTreeItem[];
+  isDirectory: boolean;
+};
+
+function parseFileTree(data: GroupedPaths, basePath = ''): FileTreeItem[] {
+  const toPath = (...parts: string[]) => {
+    return parts.filter(Boolean).join('/');
+  };
+
+  return data.map((item) => {
+    const isDirectory = Array.isArray(item) && item.length > 1;
+
+    if (isDirectory) {
+      const dirName = item[0] as string;
+      const dirPath = toPath(basePath, dirName);
+      const children = item.slice(1);
+
+      return {
+        name: dirName,
+        path: dirPath,
+        isDirectory: true,
+        children: parseFileTree(children, dirPath),
+      };
+    } else if (typeof item === 'string') {
+      return {
+        name: item,
+        path: toPath(basePath, item),
+        isDirectory: false,
+      };
+    }
+
+    return {
+      name: String(item),
+      path: toPath(basePath, String(item)),
+      isDirectory: false,
+    };
+  });
+}
+
+export function FileTree({
+  data,
+  focus,
+  className,
+  onFileClick,
+}: FileTreeProps) {
+  const items = useMemo(() => parseFileTree(data), [data]);
+
+  return (
+    <div className={cn('h-full overflow-auto', className)}>
+      <div className="h-full p-2">
+        {items.map((item) => (
+          <FileTreeNode
+            isParentExpanded={true}
+            key={item.path}
+            item={item}
+            focus={focus}
+            level={0}
+            onFileClick={onFileClick}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+type FileTreeNodeProps = {
+  item: FileTreeItem;
+  focus: string | null;
+  level: number;
+  isParentExpanded: boolean;
+  onFileClick: (path: string) => void;
+};
+
+function FileTreeNode({
+  item,
+  focus,
+  level,
+  isParentExpanded,
+  onFileClick,
+}: FileTreeNodeProps) {
+  const [isExpanded, setIsExpanded] = useState(level === 0);
+  const isFocused = focus === item.path;
+
+  useEffect(() => {
+    if (focus == null || item.isDirectory === false) {
+      return;
+    }
+
+    if (focus.startsWith(item.path)) {
+      console.log('focusedDirectory', focus, item.path);
+      setIsExpanded(true);
+    }
+  }, [focus]);
+
+  const handleClick = () => {
+    if (item.isDirectory) {
+      setIsExpanded(!isExpanded);
+    } else {
+      onFileClick(item.path);
+    }
+  };
+
+  return (
+    <div>
+      {isParentExpanded ? (
+        <div
+          className={cn(
+            'hover:bg-muted/50 flex cursor-pointer items-center rounded-md px-2 py-1',
+            isFocused && 'bg-gray-100 hover:bg-gray-100',
+          )}
+          style={{ paddingLeft: `${level * 6}px` }}
+          onClick={handleClick}
+          role="button"
+          tabIndex={0}
+          aria-expanded={item.isDirectory ? isExpanded : undefined}
+        >
+          {item.isDirectory ? (
+            <div className="flex items-center">
+              {isExpanded ? (
+                <ChevronDown className="text-muted-foreground mr-1 h-4 w-4 shrink-0" />
+              ) : (
+                <ChevronRight className="text-muted-foreground mr-1 h-4 w-4 shrink-0" />
+              )}
+              <Folder className="mr-2 h-4 w-4 shrink-0 text-blue-400" />
+              <span className="overflow-hidden text-sm text-ellipsis whitespace-nowrap">
+                {item.name}
+              </span>
+            </div>
+          ) : (
+            <div className="flex items-center" onClick={handleClick}>
+              <span className="mr-1 w-4" />
+              <File className="text-muted-foreground mr-2 h-4 w-4 shrink-0" />
+              <span className="overflow-hidden text-sm text-ellipsis whitespace-nowrap">
+                {item.name}
+              </span>
+            </div>
+          )}
+        </div>
+      ) : null}
+      {item.isDirectory && item.children && (
+        <div>
+          {item.children.map((child) => (
+            <FileTreeNode
+              key={child.path}
+              item={child}
+              focus={focus}
+              level={level + 1}
+              onFileClick={onFileClick}
+              isParentExpanded={isParentExpanded === false ? false : isExpanded}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
