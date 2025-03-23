9ca7409393b3140d442f43c0048d1a2a3d59cca2
leegeunhyeok <dev.ghlee@gmail.com>
2025-03-23 23:55:17 +0900

    perf(template): improve render list

--- packages/template/src/pages/explorer/components/dependency-tree-view.tsx
+++ packages/template/src/pages/explorer/components/dependency-tree-view.tsx
@@ -10,6 +10,8 @@ import {
 import { CornerDownRight, Network } from 'lucide-react';
 import { FileTree } from './file-tree';
 import { useState } from 'react';
+import { FixedSizeList } from 'react-window';
+import AutoSizer from 'react-virtualized-auto-sizer';
 import { bytesToText } from '@/src/utils/filesize';
 import {
   Tooltip,
@@ -36,7 +38,7 @@ export function DependencyTreeView() {
 
     if (module) {
       return (
-        <div className="flex flex-col gap-2 p-2">
+        <div className="flex h-full flex-col gap-2 p-2">
           <div className="flex w-full items-center justify-between">
             <h2 className="text-lg font-semibold">Discover module</h2>
             <Button
@@ -85,26 +87,66 @@ export function DependencyTreeView() {
   const renderDependencies = (
     imports: Metafile['metafile']['inputs'][string]['imports'],
   ) => {
+    function DependencyRow({ index, style, data }) {
+      const value = data[index];
+      const module = window.__esdoctorDataSource.metafile.inputs[value.path];
+
+      return (
+        <Tooltip key={value.path}>
+          <TooltipTrigger asChild>
+            <div
+              style={style}
+              className="hover:bg-muted/50 text-muted-foreground flex cursor-pointer items-center gap-2 rounded-md px-2 py-1"
+              onClick={() => setSelectedFile(value.path)}
+            >
+              <CornerDownRight size={16} />
+              <p className="text-sm">{value.original ?? value.path}</p>
+              {module?.format ? (
+                <Badge
+                  variant="outline"
+                  className={cn(
+                    module.format === 'esm'
+                      ? 'border-blue-300 bg-blue-100 text-blue-500'
+                      : 'border-yellow-300 bg-yellow-100 text-yellow-500',
+                  )}
+                >
+                  {module.format.toUpperCase()}
+                </Badge>
+              ) : null}
+              {module?.bytes != null ? (
+                <Badge
+                  variant="outline"
+                  className="border-gray-300 bg-gray-100 text-gray-500"
+                >
+                  {bytesToText(module.bytes)}
+                </Badge>
+              ) : null}
+            </div>
+          </TooltipTrigger>
+          <TooltipContent>{value.path}</TooltipContent>
+        </Tooltip>
+      );
+    }
+
     return imports.length === 0 ? (
       <div className="text-muted-foreground text-sm">
         No dependencies found.
       </div>
     ) : (
-      <div className="flex flex-col gap-1">
-        {imports.map((value) => (
-          <Tooltip key={value.path}>
-            <TooltipTrigger asChild>
-              <div
-                className="hover:bg-muted/50 text-muted-foreground flex cursor-pointer items-center gap-2 rounded-md px-2 py-1"
-                onClick={() => setSelectedFile(value.path)}
-              >
-                <CornerDownRight size={16} />
-                <p className="text-sm">{value.original ?? value.path}</p>
-              </div>
-            </TooltipTrigger>
-            <TooltipContent>{value.path}</TooltipContent>
-          </Tooltip>
-        ))}
+      <div className="gap- flex h-full flex-col">
+        <AutoSizer>
+          {({ width, height }) => (
+            <FixedSizeList
+              itemData={imports}
+              itemSize={30}
+              width={width}
+              height={height}
+              itemCount={imports.length}
+            >
+              {DependencyRow}
+            </FixedSizeList>
+          )}
+        </AutoSizer>
       </div>
     );
   };
@@ -125,7 +167,7 @@ export function DependencyTreeView() {
           </SheetDescription>
         </SheetHeader>
         <TooltipProvider>
-          <div className="border-border flex h-[calc(100%-82px)] border-t">
+          <div className="border-border flex h-full border-t">
             <div className="border-border h-full w-[280px] overflow-auto border-r pr-2">
               <FileTree
                 data={data}
--- yarn.config.cjs
+++ yarn.config.cjs
@@ -1,6 +1,9 @@
 /** @type {import('@yarnpkg/types')} */
 const { defineConfig } = require('@yarnpkg/types');
 
+const reactPackages = ['react', 'react-dom'];
+const reactVersion = '18.3.1';
+
 module.exports = defineConfig({
   async constraints({ Yarn }) {
     for (const dep of Yarn.dependencies({ ident: 'typescript' })) {
@@ -10,5 +13,15 @@ module.exports = defineConfig({
     for (const dep of Yarn.dependencies({ ident: '@types/node' })) {
       dep.update('^22');
     }
+
+    for (const dep of Yarn.dependencies({ ident: '@types/react' })) {
+      dep.update('18.3.18');
+    }
+
+    reactPackages.forEach((ident) => {
+      for (const dep of Yarn.dependencies({ ident })) {
+        dep.update(reactVersion);
+      }
+    });
   },
 });
